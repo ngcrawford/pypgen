a31c2ab17034bd3b2c8ec16f4b366cabf56d87c6
Nicholas Crawford <ngcrawford@gmail.com>
2012-05-25 10:58:17 -0400

    Tweaked filtering - work in progress

--- fstats2circos.examples.py
+++ fstats2circos.examples.py
@@ -1,9 +1,11 @@
-fin = open("/Users/ngcrawford/Desktop/anoMar/fstats/Dest.fstats.csv",'rU')
-fout = open("/Users/ngcrawford/Desktop/anoMar/circos/data/Dest.no_filter.fstats.txt",'w')
+fin = open("/Users/ngcrawford/Desktop/anoMar/circos/data/Amar.CAP-MAR.Gst_ext.w5000bp.csv",'rU')
+fout = open("/Users/ngcrawford/Desktop/anoMar/circos/data/Amar.CAP-MAR.Gst_ext.w5000bp.filtered.csv",'w')
 for count, line in enumerate(fin):
+	line_parts = line.strip().split(',')
+	if len(line_parts) == 3: continue
 	if count == 0: continue # skip header
 	chrm, start, depth, value = line.strip().split(',')
-	# if float(value) < 0.2: continue
+	if float(value) < 0.2: continue
 	if float(depth) < 15: continue
 	if value == 'nan': continue
 	stop = int(start) + 1
