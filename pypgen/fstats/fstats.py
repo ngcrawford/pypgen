ffe041ce25d21a1ee4425546f9caeefb9a71cbdb
Nick Crawford <ngcrawford@gmail.com>
2013-02-01 12:02:07 -0500

    Added STDEV calcuation and more documentation

--- pypgen/fstats/fstats.py
+++ pypgen/fstats/fstats.py
@@ -1,42 +1,148 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
+
 import math
 
-
+## UTIlITY STATS:
 def de_NaN_list(l):
+    """Remove NaN values from list.
+
+    Returns list without NaNs.
+
+    Parameters
+    ----------
+    a : array_like
+
+    Returns
+    -------
+    m : array_like"""
+
     return [i for i in l if math.isnan(i) != True]
 
 
-def harmonic_mean(values):
-    """calculates harmonic mean from list of integers"""
+def _mean_(l):
+    """Compute the arithmetic mean along the specified axis.
 
-    fractional_counts = sum([1.0 / v for v in values])
-    harmonic_mean = float(len(values)) / fractional_counts
+    Returns the average of the array elements.
+
+    Parameters
+    ----------
+    a : array_like
+
+    Returns
+    -------
+    mean : array_like"""
+
+    l = de_NaN_list(l)
+    mean = sum(l) * 1.0 / len(l)
+    return mean
+
+
+def _mean_variance_(l):
+    """Compute the variance of the 1D list or array.
+
+    Returns the variance of the array elements, a measure of the spread of a
+    distribution.
+
+    Parameters
+    ----------
+    a : array_like
+
+    Returns
+    -------
+    variance : array_like
+
+    Note: Doc string derived from numpy.var()"""
+
+    mean = _mean_(l)
+    var = _mean_(map(lambda x: (x - mean) ** 2, l))
+    return var
+
+
+def _stdev_(l):
+    """Compute the standard deviation along the specified axis.
+
+    Returns the standard deviation, a measure of the spread of a distribution,
+    of the array elements.
+
+    Parameters
+    ----------
+    l : array_like
+        Calculate the standard deviation of these values.
+
+    Returns
+    -------
+    standard_deviation : array_like
+
+    Note: Doc string derived from numpy.std()"""
+
+    var = _mean_variance_(l)
+    stdev = math.sqrt(var)
+    return stdev
+
+
+def harmonic_mean(l):
+    """Calculates harmonic mean from list of integers
+
+    Parameters
+    ----------
+    l : array_like
+        Calculate the harmonic mean of these values.
+
+    Returns
+    -------
+    harmonic mean : array_like"""
+
+    fractional_counts = sum([1.0 / v for v in l])
+    harmonic_mean = float(len(l)) / fractional_counts
 
     return harmonic_mean
 
 
-def harmonic_mean_chao(values):
+def harmonic_mean_chao(l):
     """Calculates the harmonic mean following the method suggested by
     Anne Chao. The formula is: 1/[(1/A)+var(D)(1/A)**3]. Used for
-    calculating multilocus Dest."""
+    calculating multilocus Dest.
 
-    count = float(len(values))
+    Parameters
+    ----------
+    l : array_like
+        Calculate Chao's harmonic mean of these values.
+
+    Returns
+    -------
+    Chao's harmonic mean : array_like"""
+
+    count = float(len(l))
     if count == 0.0:
         return 0.0
-    elif sum(values) / count == 0:
+
+    elif sum(l) / count == 0:
         return 0.0
+
     else:
-        A = sum(values) / count
-        varD = sum([(v - A) ** 2 for v in values]) / count
+        A = sum(l) / count
+        varD = sum([(v - A) ** 2 for v in l]) / count
         harmonic_mean_chao = 1 / ((1 / A) + (varD) * pow((1 / A), 3))
         return harmonic_mean_chao
 
 
 def Hs_prime_est(allele_freqs, n):
     """Calculate corrected Hs: the mean within-subpopulation
-    heterozygosity (Nei and Chesser 1983)."""
+    heterozygosity (Nei and Chesser 1983).
+
+    Parameters
+    ----------
+    allele_freqs : array_like
+        These values contain the allele freqeuncies
+    
+    n : int,float
+        The number of populations
+
+    Returns
+    -------
+    H's : array_like"""
 
     n = float(n)
     Hj = [1.0 - sum([freq ** 2 for freq in pop[:4]]) for pop in allele_freqs]
@@ -122,33 +228,40 @@ def D_est(Ht_est, Hs_est, n):
 def multilocus_Gst_est(Ht_est, Hs_est):
     """Averages Ht_est and Hs_est across loci before calculating Gst."""
 
+    stdev = _stdev_(map(Gst_est, Ht_est, Hs_est))
+
     Ht_est = sum(Ht_est) / float(len(Ht_est))
     Hs_est = sum(Hs_est) / float(len(Hs_est))
 
     if Ht_est == 0.0:
-        return 0.0
+        return (0.0, 0.0)
     else:
-        Gst_est = (Ht_est - Hs_est) / Ht_est
-        return Gst_est
+        ml_Gst_est = (Ht_est - Hs_est) / Ht_est
+        return (ml_Gst_est, stdev)
 
 
 def multilocus_G_prime_st_est(Ht_est, Hs_est, n):
     """Averages across loci before calculating G'st."""
 
+    Gst_est_list = map(Gst_est, Ht_est, Hs_est)
+    stdev = _stdev_(map(G_prime_st_est, Ht_est, Hs_est, Gst_est_list, [float(n)] * len(Ht_est)))
+
     n = float(n)
     # Ht_est_ = sum(Ht_est) / float(len(Ht_est))
     Hs_est_ = sum(Hs_est) / float(len(Hs_est))
-    G_est = multilocus_Gst_est(Ht_est, Hs_est)
+    G_est = multilocus_Gst_est(Ht_est, Hs_est)[0]
 
     if (n - 1.0) * (1.0 - Hs_est_) == 0:
-        return 0.0
+        return (0.0, 0.0)
     else:
         G_prime_st = (G_est * (n - 1.0 + Hs_est_)) / ((n - 1.0) * (1.0 - Hs_est_))
-        return G_prime_st
+        return (G_prime_st, stdev)
 
 
 def multilocus_G_double_prime_st_est(Ht_est, Hs_est, n):
     """Averages across loci before calculating G''st."""
+
+    stdev = _stdev_(map(G_double_prime_st_est, Ht_est, Hs_est, [float(n)] * len(Ht_est)))
 
     n = float(n)
     Ht_est = sum(Ht_est) / float(len(Ht_est))
@@ -156,10 +269,11 @@ def multilocus_G_double_prime_st_est(Ht_est, Hs_est, n):
 
     # G_est = multilocus_Gst_est(Ht_est,Hs_est)
     if (n * Ht_est - Hs_est) * (1.0 - Hs_est) == 0:
-        return 0.0
+        return (0.0, 0.0)
     else:
-        G_double_prime_st_est = n * (Ht_est - Hs_est) / ((n * Ht_est - Hs_est) * (1.0 - Hs_est))
-        return G_double_prime_st_est
+        ml_G_double_prime_st_est = n * (Ht_est - Hs_est) / ((n * Ht_est - Hs_est) * (1.0 - Hs_est))
+
+        return (ml_G_double_prime_st_est, stdev)
 
 
 def multilocus_D_est(Ht_est, Hs_est, n):
@@ -167,5 +281,6 @@ def multilocus_D_est(Ht_est, Hs_est, n):
 
     pairs = zip(Ht_est, Hs_est)
     Dest_values = [D_est(pair[0], pair[1], 2) for pair in pairs]
-    D_est_ = harmonic_mean_chao(Dest_values)
-    return D_est_
+    ml_D_est = harmonic_mean_chao(Dest_values)
+    stdev = _stdev_(Dest_values)
+    return (ml_D_est, stdev)
