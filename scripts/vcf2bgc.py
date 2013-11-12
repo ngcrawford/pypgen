9dcf049307b2732a1253bdcacd2ce3c03a07b4bf
Nick Crawford <ngcrawford@gmail.com>
2013-11-12 10:56:48 -0800

    Updated BGC conversion script
    Still need slice selection functionality, but otherwise works fine.

--- scripts/vcf2bgc.py
+++ scripts/vcf2bgc.py
@@ -22,7 +22,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses
 The author may be contacted at ngcrawford@gmail.com
 """
 
-import sys; import os
+import sys; 
+import os
 sys.path.insert(0, os.path.abspath('..'))
 
 import re
@@ -44,9 +45,9 @@ def get_args():
 
         python scripts/vcf2bgc.py \\
         -i pypgen/data/example.vcf.gz \\
-        -p outgroups:h665,i02-210 \\
-        pop1:c511,c512,c513,c514,c515,c563,c614,c630,c639,c640 \\
-        pop2:m523,m524,m525,m589,m675,m676,m682,m683,m687,m689 \\
+        -p mixed:h665,i02-210 \\
+        p1:c511,c512,c513,c514,c515,c563,c614,c630,c639,c640 \\
+        p2:m523,m524,m525,m589,m675,m676,m682,m683,m687,m689 \\
         -c 2 \\
         -r Chr01:1-10001 \\
         -o test
@@ -63,36 +64,41 @@ def get_args():
                         type=str,
                         help='Path to VCF file.')
 
-
     parser.add_argument('-c', '--cores',
                         required=True,
                         type=int,
                         help='Number of cores to use.')
 
     parser.add_argument('-r', '-R', '--regions',
-                    required=False,
-                    # action=FooAction, # TODO: fix this!
-                    nargs='+',
-                    help="Define a chromosomal region. \
-                          A region can be presented, for example, in the following \
-                          format: ‘chr2’ (the whole chr2), ‘chr2:1000000’ (region \
-                          starting from 1,000,000bp) or ‘chr2:1,000,000-2,000,000’ \
-                          (region between 1,000,000 and 2,000,000bp including the end \
-                          points). The coordinate is 1-based.' Multiple regions can \
-                          be submitted separated by spaces. [NOte: this is the same \
-                          format as SAMTOOLs/GATK, example text largely cribbed from \
-                          SAMTOOLs]")
+                        required=False,
+                        # action=FooAction, # TODO: fix this!
+                        nargs='+',
+                        help="Define a chromosomal region. \
+                              A region can be presented, for example, in the following \
+                              format: ‘chr2’ (the whole chr2), ‘chr2:1000000’ (region \
+                              starting from 1,000,000bp) or ‘chr2:1,000,000-2,000,000’ \
+                              (region between 1,000,000 and 2,000,000bp including the end \
+                              points). The coordinate is 1-based.' Multiple regions can \
+                              be submitted separated by spaces. [NOte: this is the same \
+                              format as SAMTOOLs/GATK, example text largely cribbed from \
+                              SAMTOOLs]")
 
     parser.add_argument('-p', '--populations',
-                        nargs=3,
+                        nargs= '+',
                         help='Names of populations and samples. \
                               The format is: "p1:sample1,sample2,.. \
                               p2:sample3,sample4,..." \
                               admixed:sample3,sample4,..." \
                               Whitespace is used to delimit populations.')
 
-    return parser.parse_args()
+    parser.add_argument("-pp","--parental-pops",
+                        type=str,
+                        nargs=2,
+                        help="Names of two populations that represent the parentals. \
+                              Note: that all other populations are therefore assumed \
+                              to be part of the admixed set.")
 
+    return parser.parse_args()
 
 
 def process_header(tabix_file):
@@ -121,13 +127,26 @@ def process_header(tabix_file):
 
 
 def get_read_depths(pop_ids, vcf_line, args):
-    pops = args.populations[pop_ids]
-    pops_snvs = {k: vcf_line[k] for k in pops}
 
+
+    # Filter out parental populations if the pop is 'admixed'
+    if pop_ids == ["mixed"]:
+        pops = {k: args.populations[k] for k in args.populations if k not in args.parental_pops}
+    else:
+        pops = {k: args.populations[k] for k in pop_ids}
+
+    # Get the genotypes
+    genotypes = {}
+    for p in pops.values():
+        for g in p:
+            genotypes[g] = vcf_line[g]
+
+    # Extract read depth information.
     read_depths = []
-    for k in pops_snvs.keys():
-        if pops_snvs[k] != None:
-            read_depths.append(pops_snvs[k]['AD'])
+    for k in genotypes.keys():
+
+        if genotypes[k] != None:
+            read_depths.append(genotypes[k]['AD'])
         else:
             read_depths.append((0, 0))
 
@@ -137,20 +156,22 @@ def get_read_depths(pop_ids, vcf_line, args):
 
 def generate_bgc_input(data):
     count, vcf_line, args = data
+    p1, p2 = args.parental_pops
 
     #Parental 1
-    p1_read_depths = get_read_depths("p1", vcf_line, args)
+    p1_read_depths = get_read_depths([p1], vcf_line, args)
 
     #Parental 2
-    p2_read_depths = get_read_depths("p2", vcf_line, args)
+    p2_read_depths = get_read_depths([p2], vcf_line, args)
 
     #Admixed
-    admixed_read_depths = get_read_depths("mixed", vcf_line, args)
+    admixed_read_depths = get_read_depths(["mixed"], vcf_line, args)
 
     # GENETIC MAP LOC
     pos_kb = vcf_line["POS"] / 1000.0
     chrm_num_id = args.header[vcf_line["CHROM"]]
-    map_info = (count, chrm_num_id, pos_kb)
+    #map_info = (chrm_num_id, pos_kb)
+    map_info = (vcf_line["CHROM"], pos_kb)
 
     return {'count': count,
             'P1': p1_read_depths,
@@ -163,6 +184,7 @@ def generate_bgc_input(data):
 def vcf_iterator(args):
 
     empty_vcf_line = make_empty_vcf_ordered_dict(args.input)
+
     for count, line in enumerate(open_vcf(args)):
 
         if line.startswith('#') is True:
@@ -171,8 +193,8 @@ def vcf_iterator(args):
         vcf_line = parse_vcf_line(line, empty_vcf_line)
 
         # APPLY BASIC FILTER
-        if vcf_line["FILTER"] != "PASS":
-            continue
+        # if vcf_line["FILTER"] != "PASS":
+        #     continue
 
         # SKIP ALLELES WITH NO INFORMATION
         # NOTE: with multi alleleic snps this may
@@ -185,18 +207,12 @@ def vcf_iterator(args):
         yield (count, vcf_line, args)
 
 
-def main():
-    pass
-
-
-
-
 if __name__ == '__main__':
 
-    main()
 
     # SETUP ARGS
     args = get_args()
+    p1, p2 = args.parental_pops
 
 
     # update args
@@ -208,31 +224,110 @@ if __name__ == '__main__':
     fixed_alleles_order = []
     vcf_count = 0
 
-    outfiles = {"P1":    open("{0}.p1.txt".format(args.output), 'w'),
-                "P2":    open("{0}.p2.txt".format(args.output), 'w'),
+    outfiles = {"P1":    open("{0}.{1}.p1.txt".format(args.output, p1), 'w'),
+                "P2":    open("{0}.{1}.p2.txt".format(args.output, p2), 'w'),
                 "mixed": open("{0}.mixed.txt".format(args.output), 'w'),
                 "map":   open("{0}.map.txt".format(args.output), 'w'),
                 }
 
-    for count, result in enumerate(map(generate_bgc_input, vcf_iterator(args))):
+    pool = multiprocessing.Pool(2)
 
-        outfiles["P1"].write('locus {0}\n'.format(result['count']))
-        for i in result['P1']:
+    #for count, result in enumerate(map(generate_bgc_input, vcf_iterator(args))):
+    for count, result in enumerate(pool.imap(generate_bgc_input, vcf_iterator(args))):
+
+        if result["map"][0] != "2":
+            continue
+
+        outfiles["P1"].write('locus {0}\n'.format(count))
+
+        for i in result["P1"]:
             outfiles["P1"].write("{0} {1}\n".format(*i))
 
-        outfiles["P2"].write('locus {0}\n'.format(result['count']))
-        for i in result['P2']:
+        outfiles["P2"].write('locus {0}\n'.format(count))
+
+        for i in result["P2"]:
             outfiles["P2"].write("{0} {1}\n".format(*i))
 
         # Update this to allow for multiple admixed populations.
-        outfiles["mixed"].write('locus {0}\n'.format(result['count']))
+        outfiles["mixed"].write('locus {0}\n'.format(count))
         outfiles["mixed"].write('pop 0\n')
-        for i in result['P1']:
+
+        for i in result['mixed']:
             outfiles["mixed"].write("{0} {1}\n".format(*i))
 
-        outfiles["map"].write("{0} {1} {2}\n".format(*result["map"]))
+        outfiles["map"].write("{0} {1} {2}\n".format(count, *result["map"]))
 
         vcf_count += 1
+
+
+
+"""
+
+qsub -V -N chr1 -pe omp 4 -l h_rt=48:00:00 -b y \
+bgc \
+-a chr1.CAP.p1.txt \
+-b chr1.MAR.p2.txt \
+-h chr1.mixed.txt \
+-M chr1.map.txt \
+-F chr1 \
+-O 0 \
+-x 10000 \
+-n 5000 \
+-p 1 \
+-q 1 \
+-N 1 \
+-m 1 \
+-D 0.5 \
+-t 5 \
+-E 0.0001 \
+-d 1 \
+-s 1 \
+-I 0 \
+-u 0.04.
+
+
+qsub -V -N chr2 -pe omp 4 -l h_rt=48:00:00 -b y \
+bgc \
+-a chr2.CAP.p1.txt \
+-b chr2.MAR.p2.txt \
+-h chr2.mixed.txt \
+-M chr2.map.txt \
+-F chr2 \
+-O 0 \
+-x 10000 \
+-n 5000 \
+-p 1 \
+-q 1 \
+-N 1 \
+-m 1 \
+-D 0.5 \
+-t 5 \
+-E 0.0001 \
+-d 1 \
+-s 1 \
+-I 0 \
+-u 0.04.
+
+
+
+python scripts/vcf2bgc.py \
+-i /Users/ngcrawford/Desktop/anoMar/RADs/RADS.BT_East.AnoCar2.0.67.dna_rm.toplevel.sample_names_only.HiQualSnps.vcf.gz \
+-pp CAP MAR \
+-p CAP:CEJ021,CEJ035,CEJ036,CEJ037,CEJ039,CEJ040,CEJ041 \
+ MAR:CEJ082,CEJ084,CEJ085,CEJ088,CEJ100,CEJ106,CEJ107,CEJ108 \
+ NoG:CEJ111,CEJ112,CEJ120,CEJ121,CEJ122,CEJ110 \
+ PdV:CJS1974,CJS1976,CJS1978,CJS1979,CJS1980,CJS1982 \
+ PaPB:CEJ092,CEJ093,CEJ094,CEJ114,CEJ115,CEJ116,CEJ117 \
+ SoRG:CJS2066,CJS2067,CJS2068,CJS2069,CJS2072,CJS2073,CJS2074 \
+ StM:CEJ028,CEJ029,CEJ030,CEJ031,CEJ032,CEJ033,CEJ034 \
+-c 2 \
+-r Chr01:1-10001 \
+-o test
+"""
+
+
+
+
 
 
 
